f68a366ab1395ce29950e54f30e314c3aab3da76
Philipp Edelmann <pedelmann@lanl.gov>
2026-03-23 17:15:08 -0600

    add new codipack version

--- var/spack/repos/builtin/packages/codipack/package.py
+++ var/spack/repos/builtin/packages/codipack/package.py
@@ -16,6 +16,7 @@ class Codipack(CMakePackage, Package):
     url = "https://github.com/SciCompKL/CoDiPack/archive/refs/tags/v2.1.0.tar.gz"
     git = "https://github.com/SciCompKL/CoDiPack.git"
 
+    version("3.0.0", sha256="cc25ae436118d1347f8efa7fd0df4c0af00b48e910133ecdbce97968084a9106")
     version("2.2.0", sha256="24e9129829588fd8965620f275e40ae3a0be3b24015bc7d7280fa5ad551c10ac")
     version("2.1.0", sha256="c8d07eb01eaa056175902d5b153b8606b05d208ff0a541d15284f4d9ff6e87c2")
     version("2.0.2", sha256="c6eecfdbf5818daf80871461f23f8a29b5b72e314d2034047d0b0fcd44744339")
